e287b8a7231923feab404985c1600e31869b5994
Jagex <>
2002-12-28 22:34:08 +0100

    Wayback Machine 20021228

---
+++ yoptin.js
@@ -0,0 +1,3 @@
+window.open('http://fs2.emv2.com/Yoptin/RNS/Survey.htm','PARTENAIRE','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=435,height=250,left=1,top=1');
+
+
